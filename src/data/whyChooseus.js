d6fdf357845eadf1ed6c6e4f820986d4c87a2374
bala-codez <mohan144004@gmail.com>
2026-01-12 21:30:27 +0530

    feat(why-choose-us): add title,descryption for why-choose-us page

---
+++ src/data/whyChooseus.js
@@ -0,0 +1,22 @@
+export const whyChooseUs = [
+  {
+    title: "Senior Engineering Lead",
+    description:
+      "Every project is overseen by engineers with 8+ years ofexperience, ensuring architectural integrity.",
+  },
+  {
+    title: "Modern Tech Stack",
+    description:
+      "We use future-ready frameworks and tools designed forperformance, security, and scalability.",
+  },
+  {
+    title: "Performance & Scalability",
+    description:
+      "Our solutions are built to handle real-world traffic, growth,and production workloads.",
+  },
+  {
+    title: "Production-Ready Standards",
+    description:
+      "We follow clean architecture, testing, and deployment bestpractices for long-term reliability.",
+  },
+];
