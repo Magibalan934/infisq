65b0396e64b6f72f63545f7d4d95dccf9d880ccd
bala-codez <mohan144004@gmail.com>
2026-01-12 14:58:43 +0530

    feat(header): adding header component

---
+++ src/components/layouts/header/Header.jsx
@@ -0,0 +1,10 @@
+import NavBar from "../navbar/NavBar";
+
+export default function Header() {
+  return (
+    // header
+    <header className="w-full h-30 flex-center">
+      <NavBar />
+    </header>
+  );
+}
