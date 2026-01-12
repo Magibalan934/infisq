f64c2f5fc62b57e1f0c7aa0b7613ce069a0b57b1
bala-codez <mohan144004@gmail.com>
2026-01-12 13:59:39 +0530

    feat(header): add nav bar label and path in navItems

---
+++ src/data/navItems.js
@@ -0,0 +1,22 @@
+export const navItems = [
+  {
+    id: 1,
+    label: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    label: "Service",
+    path: "/service",
+  },
+  {
+    id: 3,
+    label: "Process",
+    path: "/process",
+  },
+  {
+    id: 4,
+    label: "Contact",
+    path: "/contact",
+  },
+];
