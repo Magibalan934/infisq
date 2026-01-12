36bb3ef73a64d9e6038cffa0094ef9ec55d2c95c
bala-codez <mohan144004@gmail.com>
2026-01-12 20:41:22 +0530

    feat(footer): add contact details for footer component

---
+++ src/data/footerContactDetails.js
@@ -0,0 +1,37 @@
+import { MapPin, Phone, Mail, Linkedin, Icon, Instagram } from "lucide-react";
+
+export const footerContactDetails = [
+  {
+    label: "Address",
+    value: "1234 Street Name, City, State, 12345",
+    Icon: MapPin,
+  },
+  {
+    label: "Phone",
+    value: "+251 63787888686886",
+    Icon: Phone,
+  },
+  {
+    label: "Email",
+    value: "info@infisq.com",
+    Icon: Mail,
+  },
+];
+
+export const socialMediaLinks = [
+  {
+    label: "LinkedIn",
+    url: "https://www.linkedin.com/company/infisq/",
+    Icon: Linkedin,
+  },
+  {
+    label: "Instagram",
+    url: "https://www.instagram.com/infisqtech/",
+    Icon: Instagram,
+  },
+  {
+    label: "Mail",
+    url: "mailto:info@infisq.com",
+    Icon: Mail,
+  },
+];
