3b38dd76e6b1d0cbc9bba27a0bb370cb37f1edbf
bala-codez <mohan144004@gmail.com>
2026-01-13 12:47:52 +0530

    fix(components): fixing btn styles

--- src/components/ui/Btn.jsx
+++ src/components/ui/Btn.jsx
@@ -1,6 +1,6 @@
 export default function Btn({ label }) {
   return (
-    <button className="py-3 px-10 text-white  font-bold bg-gradient-primary rounded-xl cursor-pointer">
+    <button className="py-3 px-10 text-white  text-base-custom font-bold bg-gradient-primary rounded-xl cursor-pointer">
       {label}
     </button>
   );
