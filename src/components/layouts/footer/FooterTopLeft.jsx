3032e31da8e53d114122eb753812a41d05a253f0
bala-codez <mohan144004@gmail.com>
2026-01-13 13:30:20 +0530

    fix(components): fix styles in header,footer,nav bar layouts

--- src/components/layouts/footer/FooterTopLeft.jsx
+++ src/components/layouts/footer/FooterTopLeft.jsx
@@ -1,7 +1,7 @@
 export default function FooterTopLeft() {
   return (
     <div className="w-full lg:w-[30%] flex flex-col gap-5">
-      <img src="/logo-big.png" alt="logo" className="w-full" />
+      <img src="/logo.png" alt="logo" className="w-full" />
       <p className="text-justify leading-7 md:leading-8">
         A top digital marketing agency focused on driving online success. We
         specialize in social media, content creation, influencer marketing,
